11c791e362dbf7e36ccdc3eee085793190ed238b
Danila Makarov <danila.makarov@gmail.com>
2021-10-01 21:50:05 +0300

    Negative velocities solution, untested

--- eq_10.py
+++ eq_10.py
@@ -29,14 +29,28 @@ class Solver:
         :param a: Variable
         :return: Function calculation result
         """
+        if U == 0:
+            integ_func = lambda x: 2 * np.sqrt((1 - x ** 2) / (a - (1 - x ** 2) * (2 - x ** 2)))
+        else:
+            integ_func = lambda x: np.sqrt(x / (x ** 3 + (U - a - 1) * x + a))
+
         if U >= 0:
-            if U == 0:
-                integ_func = lambda x: 2 * np.sqrt((1 - x ** 2) / (a - (1 - x ** 2) * (2 - x ** 2)))
-            else:
-                integ_func = lambda x: np.sqrt(x / (x ** 3 + (U - a - 1) * x + a))
+            # equation [10]
             integ_part, _ = integrate.quad(integ_func, 0, 1)
         else:
-            raise NotImplementedError
+            # equation [14]
+            im_sq_3 = -1j*np.sqrt(3)
+            sq_3 = (-a - np.sqrt(27 * a ** 2 + 4 * (U - a - 1) ** 3)) ** (1 / 3)
+            x_max = (1 - im_sq_3) * (U - a - 1) / (3 * 2 ** (3/4) * sq_3) - (1 + im_sq_3) * sq_3 / (2 ** (4 / 3))
+            if abs(np.imag(x_max)) > 0:
+                # x_max must have no complex part
+                # TODO: use logger
+                print('WARNING, could not correctly compute x_max, complex part not 0')
+            x_max = np.real(x_max)
+
+            integ_part1, _ = integrate.quad(integ_func, 0, x_max)
+            integ_part2, _ = integrate.quad(integ_func, 0, 1)
+            integ_part = 2 * integ_part1 - integ_part1
 
         return integ_part - self.const_part
 
